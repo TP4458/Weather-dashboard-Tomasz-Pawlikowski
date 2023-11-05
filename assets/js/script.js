1ea25caae7fa816ead9a5cce06bb83962fbefb46
Tomasz Pawlikowski <tp4458@gmail.com>
2023-11-05 02:03:59 +0000

    fetch 5 day forecast data

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,7 @@ var windDispEl = $("#windDisp")
 
 
 
+
 //get longitude/latitude
 function geoLocate() {
     let requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
@@ -30,6 +31,7 @@ function geoLocate() {
             longitude = data[0].lon,
             latitude = data[0].lat
         getWeather(longitude, latitude)
+        get5Day(longitude, latitude)
         })
 }
 geoLocate()
@@ -43,26 +45,38 @@ function getWeather(longitude, latitude) {
     })
     .then(function(data){
 
-        let degrees = ""
+        //function to convert wind direction in degrees to an arrow credit: https://stackoverflow.com/a/54677081
         let angle = (data.wind.deg)
             function windDirection(angle){
             const directions = ['↑', '↗', '→', '↘', '↓', '↙', '←', '↖'];
             return directions[Math.round(angle / 45) % 8];
         }
-        degrees = windDirection(angle)
+        let degrees = windDirection(angle)
+
         cityDispEl.text(data.name)
         tempDispEl.text("Temperature: " + data.main.temp + "°C")
         humidityDispEl.text("Humidity: " + data.main.humidity + "%")
         pressureDispEl.text("Pressure: " + data.main.pressure + "HpA")
         windDispEl.text("Wind: " + data.wind.speed + degrees)
+
         console.log(data)
-        console.log(degrees)
-        
-
-
     })
 }
-// getWeather()
+function get5Day(longitude, latitude) {
+    let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+    fetch (requestUrl)
+    .then (function(response) {
+        return response.json();
+    })
+    .then (function(data) {
+        console.log(data)
+        for (let i = 0; i<=data.list.length; i+=8)
+
+        console.log(data.list[i])
+    })
+
+}
+
 
     
 
